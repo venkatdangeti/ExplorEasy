eb01abe0343bd55ebd11456809f095999694e099
Venkatdangeti <dangeti.venkat@gmail.com>
2024-02-07 22:28:56 +0000

    Added mapbox api search result and map display

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,9 @@
 const mapContainer = document.getElementById("map-container");
 const homepageContainer = document.getElementById("homepage");
+const listingcontainer = document.getElementById("listings");
 // const foodSearch = document.getElementById("btnFoodSearch");
-mapContainer.style.display = "none"
+mapContainer.style.display = "none";
+listingcontainer.style.display = "none";
 
 
 
@@ -59,11 +61,151 @@ const apiKey = "pk.eyJ1IjoibWFmZXI3NCIsImEiOiJjbHMyODV2M3YwZ3NoMmxwaTZyZWJza3Q2I
 // const queryUrl = `https://api.mapbox.com/search/searchbox/v1/?access_token=${apiKey}`;
 
 
-function displayMap( long,lat) {
+function buildLocationList(stores) {
+    const listings = document.getElementById('listings');
+    // listings.innerHTML = "";
+    for (const store of stores.features) {
+        /* Add a new listing section to the sidebar. */
+
+        const listing = listings.appendChild(document.createElement('div'));
+        /* Assign a unique `id` to the listing. */
+        listing.id = `listing-${store.id}`;
+        /* Assign the `item` class to each listing for styling. */
+        listing.className = 'item';
+
+        /* Add the link to the individual listing created above. */
+        const link = listing.appendChild(document.createElement('a'));
+        link.href = '#';
+        link.className = 'title';
+        link.id = `link-${store.id}`;
+        link.innerHTML = `${store.place_name}`;
+
+        /* Add details to the individual listing. */
+        const details = listing.appendChild(document.createElement('div'));
+        details.innerHTML = `${store.text}`;
+        if (store.properties.phone) {
+            details.innerHTML += ` · ${store.properties.phoneFormatted}`;
+        }
+        if (store.properties.distance) {
+            const roundedDistance = Math.round(store.properties.distance * 100) / 100;
+            details.innerHTML += `<div><strong>${roundedDistance} miles away</strong></div>`;
+        }
+        link.addEventListener('click', function () {
+            for (const feature of stores.features) {
+                if (this.id === `link-${feature.id}`) {
+                    flyToStore(feature);
+                    createPopUp(feature);
+                }
+            }
+            const activeItem = document.getElementsByClassName('active');
+            if (activeItem[0]) {
+                activeItem[0].classList.remove('active');
+            }
+            this.parentNode.classList.add('active');
+        });
+
+
+    }
+}
+
+function flyToStore(currentFeature) {
+    map.flyTo({
+        center: currentFeature.geometry.coordinates,
+        zoom: 15
+    });
+}
+
+function createPopUp(currentFeature) {
+    const popUps = document.getElementsByClassName('mapboxgl-popup');
+    /** Check if there is already a popup on the map and if so, remove it */
+    if (popUps[0]) popUps[0].remove();
+
+    const popup = new mapboxgl.Popup({ closeOnClick: false })
+        .setLngLat(currentFeature.geometry.coordinates)
+        .setHTML(`<h3>Sweetgreen</h3><h4>${currentFeature.properties.address}</h4>`)
+        .addTo(map);
+}
+
+function addMarkers() {
+    /* For each feature in the GeoJSON object above: */
+    for (const marker of stores.features) {
+        /* Create a div element for the marker. */
+        const el = document.createElement('div');
+        /* Assign a unique `id` to the marker. */
+        el.id = `marker-${marker.properties.id}`;
+        /* Assign the `marker` class to each marker for styling. */
+        el.className = 'marker';
+
+        /**
+         * Create a marker using the div element
+         * defined above and add it to the map.
+         **/
+        new mapboxgl.Marker(el, { offset: [0, -23] })
+            .setLngLat(marker.geometry.coordinates)
+            .addTo(map);
+    }
+}
+
+
+function showAll(data) {
+    buildLocationList(data)
     mapboxgl.accessToken = apiKey;
     const map = new mapboxgl.Map({
         container: 'map', // container ID
-        center: [ long, lat], // starting position [lng, lat]
+        zoom: 13, // starting zoom
+        center: [data.features[0].center[0], data.features[0].center[1]]
+
+    });
+    map.addControl(new mapboxgl.NavigationControl());
+
+    map.on('load', () => {
+        /* Add the data to your map as a layer */
+        map.addLayer({
+            id: 'locations',
+            type: 'circle',
+            /* Add a GeoJSON source containing place coordinates and information. */
+            source: {
+                type: 'geojson',
+                data: data
+            }
+        });
+    });
+
+    map.on('click', (event) => {
+        /* Determine if a feature in the "locations" layer exists at that point. */
+        const features = map.queryRenderedFeatures(event.point, {
+            layers: ['locations']
+        });
+
+        /* If it does not exist, return */
+        if (!features.length) return;
+
+        const clickedPoint = features[0];
+
+        /* Fly to the point */
+        flyToStore(clickedPoint);
+
+        /* Close all other popups and display popup for clicked store */
+        createPopUp(clickedPoint);
+
+        /* Highlight listing in sidebar (and remove highlight for all other listings) */
+        const activeItem = document.getElementsByClassName('active');
+        if (activeItem[0]) {
+            activeItem[0].classList.remove('active');
+        }
+        const listing = document.getElementById(
+            `listing-${clickedPoint.properties.id}`
+        );
+        listing.classList.add('active');
+    });
+    addMarkers()
+}
+
+function displayMap(long, lat) {
+    mapboxgl.accessToken = apiKey;
+    const map = new mapboxgl.Map({
+        container: 'map', // container ID
+        center: [long, lat], // starting position [lng, lat]
         zoom: 13 // starting zoom
 
     });
@@ -96,7 +238,8 @@ function getLocation(city) {
     fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${city}.json?proximity=ip&access_token=${apiKey}`)
         .then(response => response.json())
         .then(data => {
-            console.log(data, "Arr", data.features[0].center);
+            console.log("Data", data)
+            // console.log(data, "Arr", data.features[0].center);
             // let geoJson = []
             // for (let i = 0; i < data.feature.length; i++) {
             //     geoJson.push({
@@ -110,8 +253,9 @@ function getLocation(city) {
             //         }
             //     })
             // }
-           displayMap(data.features[0].center[0], data.features[0].center[1])
-           // displayMap(geoJson)
+            /// displayMap(data.features[0].center[0], data.features[0].center[1])
+            // displayMap(geoJson)
+            showAll(data)
         });
     // .then(data => {
     // const {lat, lng} = 
@@ -123,8 +267,8 @@ var btnFoodSearch = document.getElementById("btnFoodSearch");
 btnFoodSearch.addEventListener("click", function () {
     var foodCity = document.getElementById("foodCity").value;
     mapContainer.style.display = "block"
+    listingcontainer.style.display = "block";
     homepageContainer.style.display = "none"
-
     console.log(foodCity);
     getLocation(foodCity);
 });
@@ -136,6 +280,7 @@ function getFoodDrink() {
         .then(response => response.json())
         .then(data => {
             console.log(data);
+            showAll(data)
         });
 }
 getLocation(city);
@@ -146,8 +291,8 @@ var btnShoppingSearch = document.getElementById("btnShoppingSearch");
 btnShoppingSearch.addEventListener("click", function () {
     var shoppingCity = document.getElementById("shoppingCity").value;
     mapContainer.style.display = "block"
+    listingcontainer.style.display = "block";
     homepageContainer.style.display = "none"
-
     console.log(shoppingCity);
     getLocation(shoppingCity);
 });
@@ -158,6 +303,7 @@ function getShopping() {
         .then(response => response.json())
         .then(data => {
             console.log(data);
+            showAll(data)
         });
 }
 getLocation(city);
@@ -168,8 +314,8 @@ var btnNightLifeSearch = document.getElementById("btnNightLifeSearch");
 btnNightLifeSearch.addEventListener("click", function () {
     var nightLifeCity = document.getElementById("nightLifeCity").value;
     mapContainer.style.display = "block"
+    listingcontainer.style.display = "block";
     homepageContainer.style.display = "none"
-
     console.log(nightLifeCity);
     getLocation(nightLifeCity);
 });
@@ -181,9 +327,10 @@ function getNightLife() {
         .then(response => response.json())
         .then(data => {
             console.log(data);
+            showAll(data)
         });
 }
 getLocation(city);
 
-        //ACCESS TO SEARCH FOOD BTN WITH EVENT LISTENER
+//ACCESS TO SEARCH FOOD BTN WITH EVENT LISTENER
 
