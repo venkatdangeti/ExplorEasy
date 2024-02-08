529782f466eff4789f62eec0a748e8846703f945
Venkatdangeti <dangeti.venkat@gmail.com>
2024-02-08 11:18:06 +0000

    Added weather card details,README details and fixes to html

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
-# ExplorEasy
-ExplorEasy is an app where users can search for an activity/trip/night out using either their current location or selected city, based on their need and likes.
+# ExploreEasy
+
+## ExplorEasy is an app where users can search for an activity/trip/night out using their selected city.
+
+This application was created by David Brooks, Fernanda, Venkat D and Teannie Binns.
+Our motivation for this project was to allow users to easily plan a day out based on location and weather.
+This solved the problem of using multiple apps to plan a day or evening out.
+We figured out how to integrate multiple API’s into a site and get them all working in sync from one user search.
+
+## Installation
+
+There is no installation required for this application.
+
+## Usage
+
+To use this application:
+-Select which activity you would like
+-Enter the location you would like the activity to be in.
+-A map will be displayed along with the weather for that city
+ADD SCREENSHOT HERE:
+![alt text](assets/images/screenshot.png)
+
+## Credits
+
+David Brooks
+Fernanda
+Venkat D
+Teannie Binns
+API’s
+https://openweathermap.org/current#name
+https://docs.mapbox.com/api/overview/
+
+## License
+
+MIT License
+Copyright (c) [2024]
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+---
+
+openweathermap.orgopenweathermap.org
+Current weather data - OpenWeatherMap
+Access current weather data for any location on Earth. We collect and process weather data from different
+sources such as global and local weather models, satellites, radars and vast network of weather stations. Data is
+available in JSON, XML, or HTML format
+MapboxMapbox
+API Docs
+An introduction to the Mapbox web services APIs. (318 kB)
--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,65 +1,33 @@
-// API key
-var apiKey = "cead2a3023567e3257c8bf4d76c421c3";
+var weatherapiKey = "cead2a3023567e3257c8bf4d76c421c3";
 var date = dayjs().format("DD-MM-YYYY");
-const city = "CITY"
-
-
-// $(document).ready(function () {
-//   var city;
-//   var lat;
-//   // Get value on button click and show alert
-//   $("#search-button").click(function (event) {
-//     event.preventDefault();
-//     var cityInput = $("#search-input").val();
-//     // alert("this works" + cityInput);
-//     city = cityInput;
-//     displayWeather(city);
-//     createButton(city);
-//   });
-// });
-
-// displayWeather ()
-
+// const city = "CITY"
+$(document).ready(function () {
+  var city;
+  var lat;
+  // get value from button. shows in console but not console log
+  $("#btnFoodSearch").click(function (event) {
+    event.preventDefault();
+    var cityInput = $("#foodCity").val();
+    city = cityInput;
+    displayWeather(city);
+  });
+});
 function displayWeather(city) {
-  var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey + "&units=metric";
-
+  var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${weatherapiKey}&units=metric`;
   fetch(queryURL)
     .then(function (response) {
       // console.log("check then")
       return response.json();
     })
     .then(function (data) {
+      console.log(data.main.temp, "is the temp");
       showData(data);
-      console.log(data, "this is the data");
-    }
+    });
 }
-
-    // function displayWeather(city)
-
-//       fetch(
-//         "https://api.openweathermap.org/data/2.5/forecast?lat=" + data.coord.lat + "&lon=" + data.coord.lon + "&appid=" + apiKey + "&units=metric")
-//         .then(function (response) {
-//           console.log("secondapi");
-//           // does this need to be json.prson for date
-//           return response.json();
-//         })
-//         .then(function (data) {
-//           // showData(data)
-//           console.log(data, "this is 5day  data");
-//           showFiveDay(data);
-
-//     });
-// })
-// }
-
-// function showData(data) {
-// // $(".todayimg").append(data.weather[0].icon);
-//   $(".location").append(data.name + " ")
-//   $(".todaydate").append(date);
-//   $(".temp").append("Temp: " + Math.floor(data.main.temp) + "ºC");
-//   $(".wind").append ("Wind: " + data.wind.speed);
-//   $(".humidity").append("Humidity: " + data.main.humidity);
-
-// }
-
-
+function showData(data) {
+  let iconLink = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+  $("#temp").append("Temp: " + Math.floor(data.main.temp) + "ºC");
+  $('#img').append(`<img src="${iconLink}" />`)
+  $("#wind").append("Wind: " + data.wind.speed);
+  $("#humidity").append("Humidity: " + data.main.humidity);
+}
